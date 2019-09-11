8e9a7f10078c71b9147522bf5f9cf46e5ecf60b9
Rodrigo <rodrigo@infoservsoftware.com.br>
2019-09-11 17:54:35 -0300

    chore: SignIn success saga put

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,9 +1,17 @@
 import { put, call } from 'redux-saga/effects';
 import api from '../../services/api';
+import { signInSuccess } from '../actions/auth';
 
 export function* signIn({ payload }) {
     const { email, password } = payload;
 
     const response = yield call(api.post, 'session', { email, password });
     const { token, user } = response.data;
+
+    if (!user.provider) {
+        console.error('Usuário não é prestador.')
+        return;
+    }
+
+    yield put(signInSuccess(token, user));
 }

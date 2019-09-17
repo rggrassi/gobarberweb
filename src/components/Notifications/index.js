ddbfc4793e118e7495686248ae2c078c9564c3b3
Rodrigo Grassi <rodrigo@Rodrigos-MacBook.local>
2019-09-17 00:18:08 -0300

    chore: notification list style

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Badge, NotificationList, Notification } from './styles';
+import { Container, Badge, Scroll, NotificationList, Notification } from './styles';
 import { MdNotifications } from 'react-icons/md';
 
 export default function Notifications() {
@@ -9,21 +9,23 @@ export default function Notifications() {
                 <MdNotifications color='#7159c1' size={20}/>
             </Badge>
             <NotificationList>
-                <Notification>
-                    <p>Você possui um novo agendamento para amanhã</p>
-                    <time>há 2 dias</time>
-                    <button type='button'>Marcar coomo lida</button>
-                </Notification>
-                <Notification>
-                    <p>Você possui um novo agendamento para amanhã</p>
-                    <time>há 2 dias</time>
-                    <button type='button'>Marcar coomo lida</button>
-                </Notification>
-                <Notification>
-                    <p>Você possui um novo agendamento para amanhã</p>
-                    <time>há 2 dias</time>
-                    <button type='button'>Marcar coomo lida</button>
-                </Notification>
+                <Scroll>    
+                    <Notification unread>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type='button'>Marcar coomo lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type='button'>Marcar coomo lida</button>
+                    </Notification>
+                    <Notification>
+                        <p>Você possui um novo agendamento para amanhã</p>
+                        <time>há 2 dias</time>
+                        <button type='button'>Marcar coomo lida</button>
+                    </Notification>
+                </Scroll>
             </NotificationList>
         </Container>
     )
